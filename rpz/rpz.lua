cec918327dce8c4eb3153454d4c075dfe4454e64
Spirillen <pdns@protonmail.com>
2020-07-22 16:49:43 +0200

    Updated RPZ master IP

--- rpz/rpz.lua
+++ rpz/rpz.lua
@@ -1,8 +1,8 @@
 rpzFile("/etc/powerdns/rpz/my-rules.rpz")
 
 rpzMaster(
-        {"[2a01:4f9:c010:2166::53]:53",
-        "[2a01:4f8:1c1c:abe4::53]:53"
+        {"95.216.209.53:53",
+        "116.203.32.67:53"
         },
         "adult.mypdns.cloud",
         {refresh="60",
