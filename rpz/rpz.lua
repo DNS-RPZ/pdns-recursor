790f1e93db37acaaf47626c4cee56a61892c531c
Spirillen <pdns@protonmail.com>
2020-07-22 16:56:07 +0200

    Updated RPZ master

--- rpz/rpz.lua
+++ rpz/rpz.lua
@@ -6,6 +6,72 @@ rpzMaster(
         },
         "adult.mypdns.cloud",
         {refresh="60",
-        axfrTimeout="600",
+        axfrTimeout="600"
         }
 ) 
+
+rpzMaster(
+        {"95.216.209.53:53",
+        "116.203.32.67:53"
+        },
+        "adware.mypdns.cloud",
+        {refresh="60",
+        axfrTimeout="600"
+        }
+)
+
+rpzMaster(
+        {"95.216.209.53:53",
+        "116.203.32.67:53"
+        },
+        "gambling.mypdns.cloud",
+        {refresh="60",
+        axfrTimeout="600"
+        }
+)
+
+rpzMaster(
+        {"95.216.209.53",
+        "116.203.32.67"},
+        "pirated.mypdns.cloud",
+        {refresh="600",
+        axfrTimeout="600"
+        }
+)
+
+rpzMaster(
+        {"95.216.209.53",
+        "116.203.32.67"},
+        "rpz.mypdns.cloud",
+        {zoneSizeHint="650000",
+        refresh="600",
+        axfrTimeout="600"
+        }
+)
+
+rpzMaster(
+        {"95.216.209.53",
+        "116.203.32.67"},
+        "spam.mypdns.cloud",
+        {refresh="600",
+        axfrTimeout="600"
+        }
+)
+
+rpzMaster(
+        {"95.216.209.53",
+        "116.203.32.67"},
+        "tracking.mypdns.cloud",
+        {refresh="600",
+        axfrTimeout="600"
+        }
+)
+
+rpzMaster(
+        {"95.216.209.53",
+        "116.203.32.67"},
+        "typosquatting.mypdns.cloud",
+        {refresh="600",
+        axfrTimeout="600"
+        }
+)
